9b6ebe6c5c89ad720464d4249e3e3b2e972efb15
Aidriel2002 <147413687+Aidriel2002@users.noreply.github.com>
2025-05-08 08:34:23 +0000

    f

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3pWYWH0RA_Fn9Pu1ZlqqGlJiUWigYybs",
-  authDomain: "nieldo-1ef3a.firebaseapp.com",
-  projectId: "nieldo-1ef3a",
-  storageBucket: "nieldo-1ef3a.appspot.com",
-  messagingSenderId: "410082067433",
-  appId: "1:410082067433:web:d9cb3920d9f0cb3b2a0178",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,4 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.unregister();
